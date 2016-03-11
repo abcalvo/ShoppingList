1a7332531ec186421ec834cf9a916677697b4815
abcalvo <alvaro@alvarobermejo.com>
2016-03-12 00:39:33 +0100

    Product and list views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def index
+    if user_signed_in? then
+      redirect_to list_path(List.last)
+    end
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,13 +6,19 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @from_edit = params.has_key?(:from_edit)
+    @list = params[:list] || -1
   end
 
   def create
     @product = Product.create_new_product(product_params, current_user)
 
     if @product.save
-      redirect_to product_path(@product)
+      if (params[:product][:from_edit] == 'true')
+        redirect_to edit_list_path(params[:product][:list])
+      else
+        redirect_to product_path(@product)
+      end
     else
       flash.now[:warning] = 'Error creando nuevo producto'
       render 'new'
