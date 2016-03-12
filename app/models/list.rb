c46c49334f94ccc3615a51ef8fd8a4a3f03064ad
abcalvo <alvaro@alvarobermejo.com>
2016-03-12 01:12:41 +0100

    Back to list when editing list and items in alphabetical order

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ActiveRecord::Base
   belongs_to :user
-  has_many :items
+  has_many :items, -> { joins(:product).order('products.name ASC') }
 
   def self.create_new_list(list_params, owner)
     list = List.new(list_params)
