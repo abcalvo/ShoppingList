d850b37094bc41cc367ead533cbebdb719e04068
abcalvo <alvaro@alvarobermejo.com>
2016-04-20 16:09:51 +0200

    Add default on migration marked_as_default_at

--- db/migrate/20160420131119_add_marked_as_default_at_to_lists.rb
+++ db/migrate/20160420131119_add_marked_as_default_at_to_lists.rb
@@ -1,5 +1,5 @@
 class AddMarkedAsDefaultAtToLists < ActiveRecord::Migration
   def change
-    add_column :lists, :marked_as_default_at, :datetime, null: false
+    add_column :lists, :marked_as_default_at, :datetime, null: false, default: Time.now
   end
 end
